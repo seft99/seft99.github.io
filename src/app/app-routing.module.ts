c4356cbe383e59ab519e83e0e79ba3e9122f0b9e
unknown <sebas.1910@hotmail.com>
2024-05-09 17:29:32 -0500

    VERCEL

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { SPAComponent } from './pages/components/spa/spa.component';
 
 const routes: Routes = [
-{
-  path: '',
-  redirectTo:'/main',
-  pathMatch:'full'
-
-},
-{
-path:'main',
-component:SPAComponent,
-}
-
+  {
+    path: '',
+    redirectTo: '/spa',
+    pathMatch: 'full'
+  },
+  {
+    path: 'spa',
+    component: SPAComponent,
+  }
 ];
 
 @NgModule({
